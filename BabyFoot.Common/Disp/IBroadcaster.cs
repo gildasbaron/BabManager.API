c4725ff3f9bf6c1791e7cfdfe5ba21a2e03c5844
Gildas Baron <gbaron@talentsoft.com>
2020-02-20 09:03:31 +0100

    babfix

--- BabyFoot.Common/Disp/IBroadcaster.cs
+++ BabyFoot.Common/Disp/IBroadcaster.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BabyFoot.Common.Disp
+﻿namespace BabyFoot.Common.Disp
 {
     public interface IBroadcaster
     {
